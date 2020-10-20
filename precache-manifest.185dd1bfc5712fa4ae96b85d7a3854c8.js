394740bab5d1646750506e5f1f04985c2c398d35
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-10-20 13:38:38 +0300

    Updates

---
+++ precache-manifest.185dd1bfc5712fa4ae96b85d7a3854c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd600140bbd7d2e729659f507d18e14",
+    "url": "/react_redux_store/index.html"
+  },
+  {
+    "revision": "63501f935c329a6ac465",
+    "url": "/react_redux_store/static/css/main.e120c6bf.chunk.css"
+  },
+  {
+    "revision": "57e39a40049e03491bb4",
+    "url": "/react_redux_store/static/js/2.c7b43a1e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_redux_store/static/js/2.c7b43a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63501f935c329a6ac465",
+    "url": "/react_redux_store/static/js/main.241aa709.chunk.js"
+  },
+  {
+    "revision": "79afe430f8d3537e3d37",
+    "url": "/react_redux_store/static/js/runtime-main.88ea2553.js"
+  },
+  {
+    "revision": "9d3a451ccb03df0755a86238e023f6fb",
+    "url": "/react_redux_store/static/media/logo_n.9d3a451c.svg"
+  }
+]);
